9dcc0ce20dc5c54b6b363d600a120b34592f0681
Ida Hallbæck <idha0001@stud.kea.dk>
2023-11-08 13:44:09 +0100

    section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// BURGERMENU NAV
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav_menu");
 
@@ -11,3 +13,19 @@ document.querySelectorAll(".nav_link").forEach(n => n.addEventListener("click", 
   navMenu.classList.remove("active");
   
 }))
+
+// FORDELE
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const benefitLinks = document.querySelectorAll('section#membership li a');
+    const benefitContents = document.querySelectorAll('section#membership li p');
+
+    benefitLinks.forEach((link, index) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const content = benefitContents[index];
+        content.classList.toggle('show');
+      });
+    });
+  });
